1114acde319b1eebe13a0d684e254f0875d8afe8
losc06001 <losc06001@E-PW0234JE>
2023-09-15 12:50:19 +0200

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ namespace Uppgift2._3 
+{ 
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            string g = Console.ReadLine();
+
+            Console.WriteLine("Hur många kilometer kommer du köra?");
+            string z = Console.ReadLine();
+
+            double x = double.Parse(g);
+            double y = double.Parse(z);
+
+            double sum = 300 + y + x*500 -500;
+            Console.WriteLine("Din hyra för att låna bilen blir " + sum + " kr");
+            Console.ReadKey();
+
+        }
+    }
+}
